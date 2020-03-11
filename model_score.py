da9699abd6e097c49b73f302aa074affeb8f49f9
francisco-avalos <avalosjr.francisco@gmail.com>
2020-03-11 14:07:52 -0700

    adding updates

--- model_score.py
+++ model_score.py
@@ -1,16 +1,56 @@
-import main
+# import main
 import sys
 import os
 from joblib import dump, load
 
-# FILENAME = 'lr_model.sav'
+FILENAME = 'lr_model.sav'
 
-# if os.path.isfile('lr_model.sav'):
-# 	CURRENT_MODEL = load(FILENAME)
+if os.path.isfile('lr_model.sav'):
+	CURRENT_MODEL = load(FILENAME)
 
 
-print('enter something')
 if __name__ == '__main__':
-    CUSTOMER_TYPE = sys.argv[1]
+    CUSTOMER_TYPE = str(sys.argv[1])
+    QUANTITY = int(sys.argv[2])
+    CATEGORY = str(sys.argv[3])
+    WEEK_DATE = int(sys.argv[4])
+    CUSTOMER_REGION = str(sys.argv[5])
 
-    print(f"printing your input: {CUSTOMER_TYPE}")
+    print(f"printing this: {CUSTOMER_TYPE}")
+    print(f"printing this: {QUANTITY}")
+    print(f"printing this: {CATEGORY}")
+    print(f"printing this: {WEEK_DATE}")
+    print(f"printing this: {CUSTOMER_REGION}")
+
+    def customer_type(CUSTOMER_TYPE):
+    	CUSTOMER_TYPE.lower()
+    	if CUSTOMER_TYPE not in ('customer', 'corporate', 'home-office'):
+    		print('Error: Enter a valid entry (customer, corporate, or home-office)')
+    	return CUSTOMER_TYPE
+    
+    def quantity_amount(QUANTITY):
+    	if QUANTITY <= 0:
+    		print('Error: Enter a int >= 1')
+    	return QUANTITY
+    def category_type(CATEGORY):
+    	CATEGORY.lower()
+    	if CATEGORY not in ('apparel', 'electronics', 'sports'):
+    		print('Error: Invalid entry (must be apparel, electronics, sports)')
+    	return CATEGORY
+    def week_date_classifier(WEEK_DATE):
+    	if WEEK_DATE not in (1, 2, 3, 4, 5, 6, 7):
+    		print('Errror: Invalid entry. Must be between 1-7')
+    	return WEEK_DATE
+    def specify_region(CUSTOMER_REGION):
+    	CUSTOMER_REGION.lower()
+    	if CUSTOMER_REGION not in ('pacific', 'mountain', 'west north central', 'east north central',
+                                   'west south central', 'east south central', 'south atlantic',
+                                   'middle atlantic', 'new england'):
+    	    print('Erorr: Invalid entry (must a US region)')
+    	return CUSTOMER_REGION
+
+    def convert_customer_type(CUSTOMER_TYPE):
+    	
+
+
+print(CURRENT_MODEL)
