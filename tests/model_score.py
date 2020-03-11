28cfa6caf753157ca8536b6ca88c16e69f3b2c15
francisco-avalos <avalosjr.francisco@gmail.com>
2020-03-11 13:11:29 -0700

    added stuff

--- tests/model_score.py
+++ tests/model_score.py
@@ -1,1 +1,16 @@
 import main
+import sys
+import os
+from joblib import dump, load
+
+# FILENAME = 'lr_model.sav'
+
+# if os.path.isfile('lr_model.sav'):
+# 	CURRENT_MODEL = load(FILENAME)
+
+
+print('enter something')
+if __name__ == '__main__':
+    CUSTOMER_TYPE = sys.argv[1]
+
+    print(f"printing your input: {CUSTOMER_TYPE}")
