e340ccbbef776c790c1bb639eae4922c8592684b
francisco-avalos <avalosjr.francisco@gmail.com>
2020-03-11 20:49:10 -0700

    updates

--- tests/test_week_date.py
+++ tests/test_week_date.py
@@ -2,3 +2,13 @@ import pandas
 import pytest
 
 from week_date.week_date_entry import customer_type
+
+# string_test = 'apparel'
+
+# test = customer_type(string_test)
+
+
+def test_customer_type():
+	"""function to test date entries"""
+	expected_output = 'Error'
+	# output = customer_type('apparel')
