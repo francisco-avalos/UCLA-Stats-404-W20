38a2a9a0ffdc47b80e4bde1bf622ac60b880b18c
francisco-avalos <avalosjr.francisco@gmail.com>
2020-03-11 22:01:03 -0700

    test suite

--- tests/test_week_date.py
+++ tests/test_week_date.py
@@ -1,14 +1,31 @@
 import pandas
 import pytest
 
-from week_date.week_date_entry import customer_type
+from quantity_checks.numerical_entries import date_entries, quantity_entries, client_entries
 
 # string_test = 'apparel'
 
 # test = customer_type(string_test)
 
 
-def test_customer_type():
+def test_date_type():
 	"""function to test date entries"""
-	expected_output = 'Error'
-	# output = customer_type('apparel')
+	expected_output = 3
+	output = date_entries('3/1/2000 01:10')
+
+
+def test_quantity_entries():
+	"""Function to test whether orders have a 
+	   reasonable number of order items.
+	"""
+	expected_output = 5
+	output = quantity_entries(1)
+
+def quantity_integration_with_date_entries():
+	"""Integration test to check that (reasonably) as the weekened approaches, more items 
+		are bought. The purpose here is solely to check the integration between these two 
+		functions and not necessarily claim that this assumption is true.
+	"""
+	expected_output = 5
+	day_of_week = date_entries('03/13/2020 05:00')
+	items_ordered = quantity_entries(day_of_week)
