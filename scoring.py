5b4262bfb08e09f144397539f162ad0f33e49a8e
francisco-avalos <avalosjr.francisco@gmail.com>
2020-03-18 17:01:22 -0700

    entries now not convert to 1xN matrix

--- scoring.py
+++ scoring.py
@@ -37,95 +37,95 @@ if __name__ == '__main__':
     REGION = region_entry(REGION)
 
 
-    customer_entry = [[0, 0]]
+    customer_entry = [0, 0]
     if CUSTOMER == 'individual':
-        customer_entry = [[0, 0]]
+        customer_entry = [0, 0]
     elif CUSTOMER == 'business':
-        customer_entry = [[1, 0]]
+        customer_entry = [1, 0]
     elif CUSTOMER == 'home office':
-        customer_entry = [[0, 1]]
+        customer_entry = [0, 1]
 
     if QUANTITY == 1:
-        quantity_entry = [[0, 0, 0, 0]]
+        quantity_entry = [0, 0, 0, 0]
     elif QUANTITY == 2:
-        quantity_entry = [[1, 0, 0, 0]]
+        quantity_entry = [1, 0, 0, 0]
     elif QUANTITY == 3:
-        quantity_entry = [[0, 1, 0, 0, ]]
+        quantity_entry = [0, 1, 0, 0, ]
     elif QUANTITY == 4:
-        quantity_entry = [[0, 0, 1, 0]]
+        quantity_entry = [0, 0, 1, 0]
     elif QUANTITY == 5:
-        quantity_entry = [[0, 0, 0, 1]]
+        quantity_entry = [0, 0, 0, 1]
 
     if DAY_OF_WEEK == 1:
-        day_entry = [[0, 0, 0, 0, 0, 0]]
+        day_entry = [0, 0, 0, 0, 0, 0]
     elif DAY_OF_WEEK == 2:
-        day_entry = [[1, 0, 0, 0, 0, 0]]
+        day_entry = [1, 0, 0, 0, 0, 0]
     elif DAY_OF_WEEK == 3:
-        day_entry = [[0, 1, 0, 0, 0, 0]]
+        day_entry = [0, 1, 0, 0, 0, 0]
     elif DAY_OF_WEEK == 4:
-        day_entry = [[0, 0, 1, 0, 0, 0]]
+        day_entry = [0, 0, 1, 0, 0, 0]
     elif DAY_OF_WEEK == 5:
-        day_entry = [[0, 0, 0, 1, 0, 0]]
+        day_entry = [0, 0, 0, 1, 0, 0]
     elif DAY_OF_WEEK == 6:
-        day_entry = [[0, 0, 0, 0, 1, 0]]
+        day_entry = [0, 0, 0, 0, 1, 0]
     elif DAY_OF_WEEK == 7:
-        day_entry = [[0, 0, 0, 0, 0, 1]]
+        day_entry = [0, 0, 0, 0, 0, 1]
 
     if PRODUCT == 'apparel':
-        product_entry = [[0, 0]]
+        product_entry = [0, 0]
     elif PRODUCT == 'electronics':
-        product_entry = [[1, 0]]
+        product_entry = [1, 0]
     elif PRODUCT == 'sports':
-        product_entry = [[0, 1]]
+        product_entry = [0, 1]
 
     if REGION == 'pacific':
-        region_entry = [[0, 0, 0, 0, 0, 0, 0, 0]]
+        region_entry = [0, 0, 0, 0, 0, 0, 0, 0]
     elif REGION == 'east north central':
-        region_entry = [[1, 0, 0, 0, 0, 0, 0, 0]]
+        region_entry = [1, 0, 0, 0, 0, 0, 0, 0]
     elif REGION == 'east south central':
-        region_entry = [[0, 1, 0, 0, 0, 0, 0, 0]]
+        region_entry = [0, 1, 0, 0, 0, 0, 0, 0]
     elif REGION == 'middle atlantic':
-        region_entry = [[0, 0, 1, 0, 0, 0, 0, 0]]
+        region_entry = [0, 0, 1, 0, 0, 0, 0, 0]
     elif REGION == 'mountain':
-        region_entry = [[0, 0, 0, 1, 0, 0, 0, 0]]
+        region_entry = [0, 0, 0, 1, 0, 0, 0, 0]
     elif REGION == 'new england':
-        region_entry = [[0, 0, 0, 0, 1, 0, 0, 0]]
+        region_entry = [0, 0, 0, 0, 1, 0, 0, 0]
     elif REGION == 'south atlantic':
-        region_entry = [[0, 0, 0, 0, 0, 1, 0, 0]]
+        region_entry = [0, 0, 0, 0, 0, 1, 0, 0]
     elif REGION == 'west north central':
-        region_entry = [[0, 0, 0, 0, 0, 0, 1, 0]]
+        region_entry = [0, 0, 0, 0, 0, 0, 1, 0]
     elif REGION == 'west south central':
-        region_entry = [[0, 0, 0, 0, 0, 0, 0, 1]]
+        region_entry = [0, 0, 0, 0, 0, 0, 0, 1]
 
     if QUANTITY == 2 & DAY_OF_WEEK == 4:
-        interaction1 = [[1]]
+        interaction1 = [1]
     else:
-        interaction1 = [[0]]
+        interaction1 = [0]
 
     if QUANTITY == 3 & DAY_OF_WEEK == 4:
-        interaction2 = [[1]]
+        interaction2 = [1]
     else:
-        interaction2 = [[0]]
+        interaction2 = [0]
 
     if QUANTITY == 2 & DAY_OF_WEEK == 5:
-        interaction3 = [[1]]
+        interaction3 = [1]
     else:
-        interaction3 = [[0]]
+        interaction3 = [0]
 
     if QUANTITY == 3 & DAY_OF_WEEK == 5:
-        interaction4 = [[1]]
+        interaction4 = [1]
     else:
-        interaction4 = [[0]]
+        interaction4 = [0]
 
     if (REGION == 'east north central') & (PRODUCT == 'electronics'):
-        interaction5 = [[1]]
+        interaction5 = [1]
     else:
-        interaction5 = [[0]]
+        interaction5 = [0]
 
     if (CUSTOMER == 'home office') & (QUANTITY == 2):
-        interaction6 = [[1]]
+        interaction6 = [1]
     else:
-        interaction6 = [[0]]
+        interaction6 = [0]
 
     customer_entry = pd.DataFrame(customer_entry)
     product_entry = pd.DataFrame(product_entry)
