ca1bd6bbdc95e208a25684e57f307c7faf431afb
francisco-avalos <avalosjr.francisco@gmail.com>
2020-03-19 12:48:21 -0700

    more verifications to the data entry

--- quantity_checks/numerical_entries.py
+++ quantity_checks/numerical_entries.py
@@ -14,17 +14,52 @@ def quantity_entries(order_quantity):
         raise ValueError('Error: entry must be of type int')
 
 
-def date_entries(date_input):
-    """The function returns the numerical day of the week
-        given the day entry.
-    """
-    if isinstance(date_input, str):
-        date_entered = pd.to_datetime(date_input, format='%m/%d/%Y %H:%M')
-        date_entered = date_entered.strftime('%u')
-        date_entered = int(date_entered)
-        return date_entered
+def hour_check_date_entry(hour_input):
+    """The function checks whether the hour entered is valid"""
+    if (hour_input > 0) & (hour_input <= 23):
+        return True
     else:
-        raise ValueError('Error: Not a string entry')
+        raise ValueError('Error: hour entry must be between 00 and 23 (inclusive)')
+
+
+def minute_check_date_entry(minute_input):
+    """The function checks whether the minute entered is valid"""
+    if (minute_input > 0) & (minute_input <= 59):
+        return True
+    else:
+        raise ValueError("Error: minute entry must be between 00 and 59")
+
+
+def hour_check_date_entry(date_entered):
+    """The function returns the hours for the given entry"""
+    if isinstance(date_entered, str):
+        date_entered = pd.to_datetime(date_entered, format='%m/%d/%Y %H:%M')
+        converted_minute = date_entered.strftime('%M')
+        return converted_minute
+    else:
+        raise ValueError('')
+
+
+def date_entries(date_input):
+    """The function checks that the date entered is valid; if it is, it returns the numerical day of the week; otherwise
+       it returns the issue.
+    """
+    if not isinstance(date_input, str):
+        raise ValueError('Error: Please enter date as a string as "MM/DD/YYYY HH:MM" format')
+
+    date_entered = pd.to_datetime(date_input, format='%m/%d/%Y %H:%M')
+    month_entered = date_entered.strftime('%m')
+    hour_entered = date_entered.strftime('%H')
+    minutes_entered = date_entered.strftime('%M')
+
+    if not hour_check_date_entry(hour_entered):
+        return hour_check_date_entry(hour_entered)
+    if not minute_check_date_entry(minutes_entered):
+        return minute_check_date_entry(minutes_entered)
+
+    date_entered = date_entered.strftime('%u')
+    date_entered = int(date_entered)
+    return date_entered
 
 
 def client_entries(type_of_client):
