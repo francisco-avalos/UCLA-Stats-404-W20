9adf09f46e232bf0d9f7d113508a8cd516b05a9a
francisco-avalos <avalosjr.francisco@gmail.com>
2020-03-19 12:52:48 -0700

    Removed unneeded functions

--- quantity_checks/numerical_entries.py
+++ quantity_checks/numerical_entries.py
@@ -1,4 +1,3 @@
-import math
 import pandas as pd
 
 
@@ -14,32 +13,6 @@ def quantity_entries(order_quantity):
         raise ValueError('Error: entry must be of type int')
 
 
-def hour_check_date_entry(hour_input):
-    """The function checks whether the hour entered is valid"""
-    if (hour_input > 0) & (hour_input <= 23):
-        return True
-    else:
-        raise ValueError('Error: hour entry must be between 00 and 23 (inclusive)')
-
-
-def minute_check_date_entry(minute_input):
-    """The function checks whether the minute entered is valid"""
-    if (minute_input > 0) & (minute_input <= 59):
-        return True
-    else:
-        raise ValueError("Error: minute entry must be between 00 and 59")
-
-
-def hour_check_date_entry(date_entered):
-    """The function returns the hours for the given entry"""
-    if isinstance(date_entered, str):
-        date_entered = pd.to_datetime(date_entered, format='%m/%d/%Y %H:%M')
-        converted_minute = date_entered.strftime('%M')
-        return converted_minute
-    else:
-        raise ValueError('')
-
-
 def date_entries(date_input):
     """The function checks that the date entered is valid; if it is, it returns the numerical day of the week; otherwise
        it returns the issue.
@@ -48,14 +21,6 @@ def date_entries(date_input):
         raise ValueError('Error: Please enter date as a string as "MM/DD/YYYY HH:MM" format')
 
     date_entered = pd.to_datetime(date_input, format='%m/%d/%Y %H:%M')
-    # month_entered = date_entered.strftime('%m')
-    # hour_entered = date_entered.strftime('%H')
-    # minutes_entered = date_entered.strftime('%M')
-    #
-    # if not hour_check_date_entry(hour_entered):
-    #     return hour_check_date_entry(hour_entered)
-    # if not minute_check_date_entry(minutes_entered):
-    #     return minute_check_date_entry(minutes_entered)
 
     date_entered = date_entered.strftime('%u')
     date_entered = int(date_entered)
